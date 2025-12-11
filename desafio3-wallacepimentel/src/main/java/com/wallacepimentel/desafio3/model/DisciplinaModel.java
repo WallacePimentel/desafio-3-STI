31ea82689aeecfc9e5f2fd857e40991162d9e03c
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 18:10:13 -0300

    feat: adicionando DisciplinaModel

---
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/model/DisciplinaModel.java
@@ -0,0 +1,11 @@
+package com.wallacepimentel.desafio3.model;
+
+public class DisciplinaModel {
+    private final String codigoDisciplina;
+    private int cargaHoraria;
+
+    public DisciplinaModel (String codigoDisciplina, int cargaHoraria) {
+        this.codigoDisciplina = codigoDisciplina;
+        this.cargaHoraria = cargaHoraria;
+    }
+}
