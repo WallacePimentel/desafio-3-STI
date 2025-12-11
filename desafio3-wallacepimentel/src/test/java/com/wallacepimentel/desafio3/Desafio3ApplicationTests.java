56010b9402ba9ce0679cc6f24bb26e0aab1b7819
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 17:16:36 -0300

    Initial commit

---
+++ desafio3-wallacepimentel/src/test/java/com/wallacepimentel/desafio3/Desafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallacepimentel.desafio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
