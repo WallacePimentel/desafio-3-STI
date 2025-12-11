580dd017cea0363dd3ac4ce6e82529cdea1b6759
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 20:34:17 -0300

    feat: criando funções básicas de adição e busca e também função de cálculo do CR do curso

--- desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/service/CursoService.java
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/service/CursoService.java
@@ -1,4 +1,43 @@
 package com.wallacepimentel.desafio3.service;
 
+import com.wallacepimentel.desafio3.model.AlunoModel;
+import com.wallacepimentel.desafio3.model.CursoModel;
+
+import java.util.HashMap;
+
 public class CursoService {
+
+    private final HashMap<String, CursoModel> cursos = new HashMap<>();
+    private AlunoService alunoService;
+
+    public CursoService (AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    public void adicionarCurso (CursoModel curso) {
+        this.cursos.put(curso.getCodigoCurso(), curso);
+    }
+
+    public CursoModel buscarCursoPorCodigo (String codigoCurso) {
+        return this.cursos.get(codigoCurso);
+    }
+
+    public boolean existeCurso (String codigoCurso) {
+        return this.cursos.containsKey(codigoCurso);
+    }
+
+    public double calcularCRCurso (CursoModel curso) {
+        int totalAlunos = curso.getAlunosDoCurso().size();
+        double somatorioCRs  = 0;
+
+        for (AlunoModel aluno : curso.getAlunosDoCurso()) {
+            somatorioCRs += alunoService.calcularCRAluno(aluno);
+        }
+
+        if (totalAlunos == 0) {
+            return 0;
+        }
+
+        return somatorioCRs / totalAlunos;
+    }
 }
