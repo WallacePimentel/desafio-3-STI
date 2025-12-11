f460abc463e15026e0bc82767d465611690803eb
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 17:59:04 -0300

    feat: criando AlunoModel

---
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/model/AlunoModel.java
@@ -0,0 +1,11 @@
+package com.wallacepimentel.desafio3.model;
+
+public class AlunoModel {
+    private String matriculaAluno;
+    private String codigoCurso;
+
+    public AlunoModel (String matriculaAluno, String codigoCurso) {
+        this.matriculaAluno = matriculaAluno;
+        this.codigoCurso = codigoCurso;
+    }
+}
