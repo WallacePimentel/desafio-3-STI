648424492279336b1cf8666be5dd3ca24f194cdf
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 18:16:02 -0300

    feat: adicionando lista de disciplinas em AlunoModel

--- desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/model/AlunoModel.java
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/model/AlunoModel.java
@@ -1,11 +1,16 @@
 package com.wallacepimentel.desafio3.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlunoModel {
     private final String matriculaAluno;
     private String codigoCurso;
+    private List<DisciplinaModel> disciplinasDoAluno;
 
     public AlunoModel (String matriculaAluno, String codigoCurso) {
         this.matriculaAluno = matriculaAluno;
         this.codigoCurso = codigoCurso;
+        this.disciplinasDoAluno = new ArrayList<>();
     }
 }
