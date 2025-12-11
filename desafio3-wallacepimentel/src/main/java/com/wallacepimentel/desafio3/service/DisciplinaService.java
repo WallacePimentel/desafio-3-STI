4e9816222e52d151bdce7cb7476c2dc2239052c6
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 19:53:09 -0300

    feat: criando DisciplinaService para lidar com adição e busca de disciplinas por codigo

---
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/service/DisciplinaService.java
@@ -0,0 +1,21 @@
+package com.wallacepimentel.desafio3.service;
+
+import com.wallacepimentel.desafio3.model.DisciplinaModel;
+
+import java.util.HashMap;
+
+public class DisciplinaService {
+    private final HashMap<String, DisciplinaModel> disciplinas = new HashMap<>();
+
+    public void adicionarDisciplina(DisciplinaModel disciplina) {
+        this.disciplinas.put(disciplina.getCodigoDisciplina(), disciplina);
+    }
+
+    public DisciplinaModel buscarDisciplinaPorCodigo (String codigoDisciplina) {
+        return disciplinas.get(codigoDisciplina);
+    }
+
+    public boolean existeDisciplina (String codigoDisciplina) {
+        return disciplinas.containsKey(codigoDisciplina);
+    }
+}
