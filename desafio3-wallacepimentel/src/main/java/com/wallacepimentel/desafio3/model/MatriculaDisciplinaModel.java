2394f3c914987890007b5a151a72b11bc4b85a06
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 18:20:22 -0300

    feat: adicionando MatriculaDisciplinaModel

---
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/model/MatriculaDisciplinaModel.java
@@ -0,0 +1,16 @@
+package com.wallacepimentel.desafio3.model;
+
+public class MatriculaDisciplinaModel {
+    private String matriculaAluno;
+    private String codigoDisciplina;
+    private int nota;
+    private String anoSemestre;
+
+    //Inicializando a nota com 0 para caso de abandono
+    public MatriculaDisciplinaModel(String matriculaAluno, String codigoDisciplina, String anoSemestre) {
+        this.matriculaAluno = matriculaAluno;
+        this.codigoDisciplina = codigoDisciplina;
+        this.nota = 0;
+        this.anoSemestre = anoSemestre;
+    }
+}
