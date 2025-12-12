6a5bb16b0199d1d7b0f651a0dc7faf28aed7f775
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-11 21:08:04 -0300

    feat: adicionando endpoints REST

--- desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/controller/CRAlunoControler.java
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/controller/CRAlunoControler.java
@@ -2,12 +2,14 @@ package com.wallacepimentel.desafio3.controller;
 
 import com.wallacepimentel.desafio3.DTO.CRAlunoDTO;
 import com.wallacepimentel.desafio3.service.AlunoService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/alunos")
 public class CRAlunoControler {
     private final AlunoService alunoService;
 
--- desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/controller/CRCursoController.java
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/controller/CRCursoController.java
@@ -2,11 +2,13 @@ package com.wallacepimentel.desafio3.controller;
 
 import com.wallacepimentel.desafio3.DTO.CRCursoDTO;
 import com.wallacepimentel.desafio3.service.CursoService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/cursos")
 public class CRCursoController {
 
     private final CursoService cursoService;
