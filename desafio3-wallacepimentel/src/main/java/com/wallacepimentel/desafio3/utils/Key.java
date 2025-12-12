adb9b8a5625f5b226fbeb1628700cc807d0714f4
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-12 13:51:03 -0300

    feat: criando classe chave para usar chave dupla para MatriculaDisciplinaModel

---
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/utils/Key.java
@@ -0,0 +1,19 @@
+package com.wallacepimentel.desafio3.utils;
+
+public class Key {
+    private String key1;
+    private String key2;
+
+    public Key (String key1, String key2) {
+        this.key1 = key1;
+        this.key2 = key2;
+    }
+
+    public String getKey1() {
+        return key1;
+    }
+
+    public String getKey2() {
+        return key2;
+    }
+}
