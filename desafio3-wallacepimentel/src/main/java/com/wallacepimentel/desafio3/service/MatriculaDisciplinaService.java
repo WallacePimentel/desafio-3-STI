b0bda4295d2b29478389066e9bce9852cdf49140
Wallace da Rocha Pimentel Júnior <wallace.r.p.jr@gmail.com>
2025-12-12 13:51:40 -0300

    feat: criando MatriculaDisciplinaService e suas operações básicas

---
+++ desafio3-wallacepimentel/src/main/java/com/wallacepimentel/desafio3/service/MatriculaDisciplinaService.java
@@ -0,0 +1,24 @@
+package com.wallacepimentel.desafio3.service;
+
+import com.wallacepimentel.desafio3.model.MatriculaDisciplinaModel;
+import com.wallacepimentel.desafio3.utils.Key;
+
+import java.util.HashMap;
+
+public class MatriculaDisciplinaService {
+
+    private final HashMap<Key, MatriculaDisciplinaModel> matriculasDeDisciplinas = new HashMap<>();
+
+    public void adicionarMatriculaDisciplina (Key keyMatriculaDisciplina,
+                                              MatriculaDisciplinaModel matriculaDisciplina) {
+        this.matriculasDeDisciplinas.put(keyMatriculaDisciplina, matriculaDisciplina);
+    }
+
+    public boolean existeMatriculaDisciplina (Key keyMatriculaDisciplina) {
+        return this.matriculasDeDisciplinas.containsKey(keyMatriculaDisciplina);
+    }
+
+    public MatriculaDisciplinaModel buscarMatriculaDisciplina (Key keyMatriculaDisciplina) {
+        return this.matriculasDeDisciplinas.get(keyMatriculaDisciplina);
+    }
+}
